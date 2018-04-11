be65e364f930eb55d38015630ff6438cc7aabbe5
orangegrove1955 <orangegrove_1955@hotmail.com>
2018-04-11 18:14:42 +1000

    Updated Book class to include all getters and setters, as well as increasing summary

--- app/src/main/java/Book.java
+++ app/src/main/java/Book.java
@@ -7,26 +7,44 @@ public class Book {
     private String mTitle;
     private int mISBN;
     private String mAuthor;
+    private String mPublisher;
+    private int mEdition;
+    private int mYearPub;
+    private String mGenre;
+    private String mBookDesc;
 
     // Default Book constructor
     public Book(){
         this.mTitle = "Book Title";
         this.mISBN = 00000000;
         this.mAuthor = "John Doe";
+        this.mPublisher = "Publisher";
+        this.mEdition = 1;
+        this.mYearPub = 2000;
+        this.mGenre = "None";
+        this.mBookDesc = "This is a book";
 
     }
 
     // Specific Book constructor
-    public Book(String title, int ISBN, String author){
+    public Book(String title, int ISBN, String author, String publisher, int edition,
+                int yearPub, String genre, String bookDesc){
         this.mTitle = title;
         this.mISBN = ISBN;
         this.mAuthor = author;
+        this.mPublisher = publisher;
+        this.mEdition = edition;
+        this.mYearPub = yearPub;
+        this.mGenre = genre;
+        this.mBookDesc = bookDesc;
     }
 
     // Summarise Book
     public String getSummary(){
         return "Title: " + this.mTitle + ", ISBN: " + this.mISBN +
-                ", Author: " + this.mAuthor;
+                ", Author: " + this.mAuthor + ", Publisher: " + this.mPublisher +
+                ", Edition: " + this.mEdition + ", Year of Publication: " + this.mYearPub +
+                ", Genre: " + this.mGenre + ", Book Description: " + this.mBookDesc;
     }
 
     // Setter Methods
@@ -47,6 +65,31 @@ public class Book {
         this.mAuthor = author;
     }
 
+    // Set publisher
+    public void setPublisher(String publisher){
+        this.mPublisher = publisher;
+    }
+
+    // Set edition
+    public void setEdition(int edition){
+        this.mEdition = edition;
+    }
+
+    // Set year of publication
+    public void setYearPub(int yearPub){
+        this.mYearPub = yearPub;
+    }
+
+    // Set genre
+    public void setGenre(String genre){
+        this.mGenre = genre;
+    }
+
+    // Set Book description
+    public void setBookDesc(String bookDesc){
+        this.mBookDesc = bookDesc;
+    }
+
     // Getter methods
     // --------------
 
@@ -64,4 +107,27 @@ public class Book {
     public String getAuthor(){
         return this.mAuthor;
     }
+
+    // Retrieve Publisher
+    public String getPublisher(){
+        return this.mPublisher;
+    }
+
+    // Retrieve edition
+    public int getEdition(){
+        return this.mEdition;
+    }
+
+    // Retrieve year of publication
+    public int getYearPub(){
+        return this.mYearPub;
+    }
+
+    // Retrieve genre
+    public String getGenre(){
+        return this.mGenre;
+    }
+
+    // Retrieve Book description
+    public String getBookDesc() { return this.mBookDesc; }
 }
